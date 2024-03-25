228b8012dfae2cd8372d25a9a5c00e79d163eec9
f2hard3 <f2hard3@gmail.com>
2024-03-26 06:49:20 +0900

    Add table view

--- bank/bank/AccountSummary/AccountSummaryViewController.swift
+++ bank/bank/AccountSummary/AccountSummaryViewController.swift
@@ -8,7 +8,47 @@
 import UIKit
 
 class AccountSummaryViewController: UIViewController {
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
+
+// MARK: - UITableView
+extension AccountSummaryViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
+}
+
